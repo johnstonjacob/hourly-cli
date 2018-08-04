6e0482b4a6a1b1a1edacc60e8fb9121b60aeda37
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-03 19:35:47 -0500

    mocha setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,13 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    mocha: true,
   },
   extends: ['prettier', 'airbnb/base'],
+  plugins: ['mocha'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'mocha/no-exclusive-tests': 'error',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'arrow-parens': [2, 'always'],
     'no-param-reassign': [
