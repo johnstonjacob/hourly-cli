c5edfa83a616870148da8c592da84bf43bf7cec4
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-03 20:57:08 -0500

    timeToHHMM tests

--- src/lib/services/helpers.js
+++ src/lib/services/helpers.js
@@ -1,7 +1,8 @@
 function minutesToHHMM(mins) {
+  if (mins <= 0) return '00:00';
+
   let mm = mins % 60;
   let hh = (mins - mm) / 60;
-
   if (mm.toString().length < 2) mm = `0${mm.toString()}`;
   if (hh.toString().length < 2) hh = `0${hh.toString()}`;
 
