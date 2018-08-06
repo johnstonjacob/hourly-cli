7fa0a6cdf6790afdf4b21c30b721b54fd58891fd
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-05 22:33:50 -0500

    standardize output colours / define structure for output

--- src/lib/commands/stop.js
+++ src/lib/commands/stop.js
@@ -1,4 +1,4 @@
-const chalk = require('chalk');
+const { success, error } = require('../services/output');
 const { isCurrentBillable, stopBillable } = require('../services/db');
 const { text } = require('../constants.json');
 const { hhmmToMs } = require('../services/helpers');
@@ -6,7 +6,7 @@ const { hhmmToMs } = require('../services/helpers');
 const stopHandler = async (time) => {
   const { milliseconds, ok } = hhmmToMs(time);
   if (!ok) {
-    console.log(chalk.red(text.invalid_param));
+    error(text.invalid_param);
     process.exit(1);
   }
   const currentBillable = await isCurrentBillable();
@@ -14,9 +14,9 @@ const stopHandler = async (time) => {
   const endTime = Date.now() - milliseconds;
 
   if (currentBillable.ok) {
-    await stopBillable(currentBillable.id, endTime);
-    console.log(chalk.green(text.stopped));
-  } else console.log(chalk.yellow(text.no_timer_running));
+    stopBillable(currentBillable.id, endTime);
+    success(text.stopped);
+  } else error(text.no_timer_running);
 };
 
 module.exports.stopHandler = stopHandler;
