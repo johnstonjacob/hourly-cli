4b2df9def8d62e2092ce17454651e04f3757e49b
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-06 21:59:02 -0500

    ramda refactor

--- src/lib/services/helpers.js
+++ src/lib/services/helpers.js
@@ -1,6 +1,4 @@
-const curry = require('lodash/fp/curry');
-const compose = require('lodash/fp/compose');
-const map = require('lodash/fp/map');
+const { curry, map, compose } = require('ramda');
 
 const formatHHMM = ([hh, mm]) => `${hh}:${mm}`;
 const leftPad = (x) => (x.toString().length < 2 ? `0${x.toString()}` : x.toString());
@@ -8,33 +6,24 @@ const getHoursAndMins = (x) => [(x - (x % 60)) / 60, x % 60];
 const removeNegative = (x) => (x <= 0 ? 0 : x);
 const minutesToHHMM = compose(formatHHMM, map(leftPad), getHoursAndMins, removeNegative);
 
-const ok = (x) => (x === 'notok' ? 'notok' : x);
+const ok = (x) => (x === 'invalid' ? 'invalid' : x);
 
 const validHHMMRegex = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-const validHHMM = (x) => (validHHMMRegex.test(x) ? x : 'notok');
+const validHHMM = (x) => (validHHMMRegex.test(x) ? x : 'invalid');
 
 const split = curry((x, a) => a.split(x));
-const splitColon = (x) => (ok(x) ? split(':', x) : 'notok');
+const splitColon = (x) => (ok(x) ? split(':', x) : 'invalid');
 
 const getHourMs = (x) => x * 3600000;
 const getMinuteMs = (x) => x * 60000;
-const getTotalMs = ([hh, mm]) => (ok(hh) ? getHourMs(hh) + getMinuteMs(mm) : 'notok');
+const getTotalMs = ([hh, mm]) => (ok(hh) ? getHourMs(hh) + getMinuteMs(mm) : 'invalid');
 
 const formatResponse = (x) => (ok(x) ? {
   milliseconds: x,
   ok: true,
 } : { ok: false });
+
 const hhmmToMs = compose(formatResponse, getTotalMs, splitColon, validHHMM);
-
-
-// function hhmmToMs(hhmm) {
-//   const hhmmRegex =
-//   if (!hhmmRegex.test(hhmm)) return { ok: false };
-
-//   const [hh, mm] = hhmm.split(':');
-
-//   return { ok: true, milliseconds: hh * 3600000 + mm * 60000 };
-// }
 
 module.exports = {
   minutesToHHMM,
--- src/lib/services/output.js
+++ src/lib/services/output.js
@@ -1,14 +1,13 @@
 const chalk = require('chalk');
-const compose = require('lodash/fp/compose');
-const curryRight = require('lodash/fp/curryRight');
+const { compose, curry } = require('ramda');
 
 const log = (output) => console.log(output, '\n');
 
 const success = compose(log, chalk.green);
 const successBold = compose(success, chalk.bold);
 const warning = compose(log, chalk.yellow);
-const stringify = curryRight(JSON.stringify);
-const prettifyJson = stringify(undefined, 2);
+const stringify = curry(JSON.stringify);
+const prettifyJson = (x) => stringify(x, undefined, 2);
 const jsonOut = compose(log, chalk.cyan.bold, prettifyJson);
 const error = compose(log, chalk.red);
 
