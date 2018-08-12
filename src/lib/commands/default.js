defea2b7c52e79402ce16f9a20fe262313e17e19
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-11 23:38:02 -0500

    end refactor

--- src/lib/commands/default.js
+++ src/lib/commands/default.js
@@ -6,17 +6,6 @@ const { isCurrentBillable, stopBillable, startBillable } = require('../services/
 
 const isOk = propEq('ok', true);
 
-// const defaultHandler = async () => {
-//   const currentBillable = await isCurrentBillable();
-//   if (currentBillable.ok) {
-//     await stopBillable(currentBillable.id);
-//     success('Billable hours stopped');
-//   } else {
-//     await startBillable();
-//     success('Billable hours started');
-//   }
-// };
-
 const started = () => success('Billable hours started.');
 const stopped = () => success('Billable hours stopped.');
 
--- src/lib/commands/end.js
+++ src/lib/commands/end.js
@@ -1,12 +1,11 @@
+const { composeP } = require('ramda');
 const { success } = require('../services/output');
 const { endBillablePeriod } = require('../services/db');
 const { text } = require('../constants.json');
 
+const endOutput = () => success(text.ended);
 
-const endHandler = async () => {
-  endBillablePeriod();
-  success(text.ended);
-};
+const endHandler = composeP(endOutput, endBillablePeriod);
 
 
 module.exports.endHandler = endHandler;
